bccba28756af502490c6fdc90588cbbbf1f4e23e
Jack-Tillman <johntillman345@gmail.com>
2023-05-27 18:33:33 -0400

    Attempted onSubmit handler, added potential issues for why it doesn't work yet, changed the value of all 3 onChanges to respond to the event, changed the .maps so that the name of each option shows up in dropdown menu

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
+
 /**
  * Don't touch these imports!
  */
@@ -11,8 +12,12 @@ import {
 
 const Search = (props) => {
   // Make sure to destructure setIsLoading and setSearchResults from the props
-  const { setIsLoading, setSearchResults } = props;
-  const [inputValue, setInputValue] = useState();
+  // const [inputValue, setInputValue] = useState();
+
+  //05/27/23 - I think this is proper destructuring, but I am not 100% certain because of the error that 'setIsLoading' is not a function... 
+
+  const {setIsLoading, setSearchResults} = props;
+
   const [centuryList, setCenturyList] = useState([]);
   const [classificationList, setClassificationList] = useState([]);
   const [queryString, setQueryString] = useState("");
@@ -38,12 +43,14 @@ const Search = (props) => {
    * 
    * Make sure to console.error on caught errors from the API methods.
    */
-
+  
+  //05/27/23 - I am pretty sure this is correct. If not, we may need another .then() to convert resCenturies into json 
   useEffect(() => {
     Promise.all([
       fetchAllCenturies(),
       fetchAllClassifications()
     ])
+    //resCenturies is the returned list of centuries, resClassifications is returned list of classifications from above 
     .then(([resCenturies, resClassifications]) => {
       setCenturyList(resCenturies);
       setClassificationList(resClassifications)
@@ -69,8 +76,39 @@ const Search = (props) => {
    * 
    * finally: call setIsLoading, set it to false
    */
+
+  /* 05/27/23 Jack 
+  This is the current obstacle. When you search something, a TypeError is thrown, saying that 'setIsLoading is not a function'. If that is temporarily resolved, 
+  another error is thrown claiming that 'setSearchResults is not a function', so I think the issue lies either in how both methods are passed as props or how they were declared initially
+
+  Potential reasons:
+  1) in the root index.js,  setIsLoading + setSearchResults is not declared properly in the return statement for the App component
+  2) In this file, Search.js, setIsLoading + setSearchResults is not properly destructured or they were not properly passed into the Search component
+
+  Other potential issues: These may also be potential reasons (but indirectly related) that onSubmit handler doesn't work correctly yet. 
+  If the above 2 reasons don't pan out at all, perhaps review the code I highlight below to ensure I didn't do anything improperly
+
+  1) In the onSubmit handler below, it is an asynchronous function, but I am not confident that I have set it up properly. 
+  2) onChange (lines 116, 125, 142) may need to be changed. 
+  
+
+
+  */
   return <form id="search" onSubmit={async (event) => {
     // write code here
+    event.preventDefault();
+    setIsLoading(true);
+
+    try{
+      const response = await fetchQueryResults({ century, classification, queryString });
+      const queryResults = response.json();
+      setSearchResults(queryResults);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+
   }}>
     <fieldset>
       <label htmlFor="keywords">Query</label>
@@ -79,7 +117,7 @@ const Search = (props) => {
         type="text" 
         placeholder="enter keywords..." 
          value={queryString} 
-        onChange={setQueryString}
+        onChange={(e) => setQueryString(e.target.value)}
         />
     </fieldset>
     <fieldset>
@@ -88,13 +126,13 @@ const Search = (props) => {
         name="classification"
         id="select-classification"
         value={classification} 
-        onChange={setClassification}
+        onChange={(e) => setClassification(e.target.value)}
         >
         <option value="any">Any</option>
         {/* map over the classificationList, return an <option /> */
-        /*if there are any issues with this, on line 89, include other properties from https://github.com/harvardartmuseums/api-docs/blob/master/sections/classification.md */
+        /*if there are any issues with this, on line 133, include other properties from https://github.com/harvardartmuseums/api-docs/blob/master/sections/classification.md */
          classificationList.map(option => {
-          return <option key={option.id} />
+          return <option key={option.id} value={option.id}>{option.name}</option>
         })}
        
       </select>
@@ -105,13 +143,15 @@ const Search = (props) => {
         name="century" 
         id="select-century"
         value={century} 
-        onChange={setCentury}
+        onChange={(e) => setCentury(e.target.value)}
         >
         <option value="any">Any</option>
         {/* map over the centuryList, return an <option /> 
-        if there are any issues with this, on line 106, include other properties from https://github.com/harvardartmuseums/api-docs/blob/master/sections/century.md */
+        if there are any issues with this, on line 151, include other properties from https://github.com/harvardartmuseums/api-docs/blob/master/sections/century.md */
         centuryList.map(option => {
-          return <option key={option.id} />
+          return (
+            <option key={option.id} value={option.id}>{option.name}</option>
+          )
         })}
         
       </select>
@@ -123,34 +163,7 @@ const Search = (props) => {
 export default Search;
 
 
-useEffect(() => {
-  let promises = [];
-  data.forEach((item) => {
-    promises.push(
-      fetch(
-        `https://app.subsocial.network/subid/api/v1/check/${item.name.toLowerCase()}`
-      )
-    );
-  });
 
-  Promise.all(promises)
-    .then((responses) => {
-      // Take all responses and analyze them with another Promise.all
-      return Promise.all(
-        responses.map((response) => {
-          return response.json();
-        })
-      );
-    })
-    .then((data) => {
-      // Extracted data
-      console.log(data);
-    })
-    .catch((error) => {
-      // Catched errors
-      console.log(error);
-    });
-}, [data]);
 /*
 
 */
